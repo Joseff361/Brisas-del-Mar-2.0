e178f14318f329ce2c5118b6cb0215850632be64
Joseff361 <Joseff361@outlook.com>
2022-01-11 14:13:49 -0500

    Adding map and its coordinates

--- src/app/components/eTable-administracion/main/main.component.ts
+++ src/app/components/eTable-administracion/main/main.component.ts
@@ -1,50 +1,63 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/domain/User';
-import { UsuarioService } from 'src/app/services/administracion/administracion-usuarios/usuarios.service';
-import { Path } from 'src/app/infrastructure/constans/Path';
-import { Configuracion } from 'src/app/domain/Configuracion';
-import { SistemaGeneralService } from 'src/app/services/administracion/sistema/sistema-general.service';
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { User } from "src/app/domain/User";
+import { UsuarioService } from "src/app/services/administracion/administracion-usuarios/usuarios.service";
+import { Path } from "src/app/infrastructure/constans/Path";
+import { Configuracion } from "src/app/domain/Configuracion";
+import { SistemaGeneralService } from "src/app/services/administracion/sistema/sistema-general.service";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.css"],
 })
 export class MainComponent implements OnInit {
+  @ViewChild('peruMap', {static: true}) peruMap: ElementRef; 
 
   public load: boolean;
   public loading: string;
   public authentication: boolean;
   public user: User;
-  public cliente :boolean;
+  public cliente: boolean;
   public configuracion: Configuracion;
-  constructor(private service: UsuarioService, private config: SistemaGeneralService) { 
+  constructor(
+    private service: UsuarioService,
+    private config: SistemaGeneralService
+  ) {
     this.user = new User();
     this.configuracion = new Configuracion();
     this.load = true;
     this.loading = Path.loading;
-    this.cliente= true;
+    this.cliente = true;
   }
 
+  imageSvg: unknown;
+
   ngOnInit() {
-    const auth = localStorage.getItem('authentication');
+    const auth = localStorage.getItem("authentication");
     this.getAuth(auth);
+
+    this.imageSvg = this.peruMap.nativeElement;
+    console.log('imageSvg', this.imageSvg)
+    
+    if (window.addEventListener) {
+      window.addEventListener("storage", this.changeColor, false);
+  }
   }
 
   private getConfiguracion() {
-    this.config.getConfiguracionSistemaGeneral().subscribe(data => {
+    this.config.getConfiguracionSistemaGeneral().subscribe((data) => {
       this.configuracion = data;
       if (this.configuracion.empnombre == null) {
-        this.configuracion.empnombre = 'Sin nombre';
+        this.configuracion.empnombre = "Sin nombre";
       }
       if (this.configuracion.empdescripcion == null) {
-        this.configuracion.empdescripcion = 'Sin descripcion';
+        this.configuracion.empdescripcion = "Sin descripcion";
       }
     });
   }
 
   getAuth(auth: string) {
-    if (auth === 'true') {
+    if (auth === "true") {
       this.getUserName();
       this.authentication = true;
     } else {
@@ -54,19 +67,26 @@ export class MainComponent implements OnInit {
 
   private getUserName() {
     this.getConfiguracion();
-    this.user.nickname = localStorage.getItem('nickname').toString();
-    this.user.password = localStorage.getItem('password').toString();
-    this.service.getUsuarioByAuthentication(this.user).subscribe(o => {
-      if (o !== null) {
-        this.load = false;
-        this.user = o;
+    this.user.nickname = localStorage.getItem("nickname").toString();
+    this.user.password = localStorage.getItem("password").toString();
+    this.service.getUsuarioByAuthentication(this.user).subscribe(
+      (o) => {
+        if (o !== null) {
+          this.load = false;
+          this.user = o;
+        }
+      },
+      (error) => {
+        if (error) {
+          this.authentication = false;
+          localStorage.clear();
+        }
       }
-    }, error => {
-      if (error) {
-        this.authentication = false;
-        localStorage.clear();
-      }
-    });
+    );
   }
 
+  /* the coolest code */
+  changeColor(): void {
+    console.log('click!!')
+  }
 }
